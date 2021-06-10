42083d2af1a9820d242bfdf47df44c9ff36fab1f
turnes <rafael@turnes.com.br>
2021-06-10 10:24:03 -0300

    Endpoints for Application Status and  Logging working

--- exercises/README.md
+++ exercises/README.md
@@ -1,1 +1,21 @@
+# Lesson 2
+## Exercise Endpoints for Application Status
 
+```bash
+
+#DEV
+docker build --target dev  -t cff-endpoints:dev .
+#PROD
+docker build -t cff-endpoints:prod  .
+
+# live coding
+docker run --rm --name flask-endpoints -i -t -p 5000:5000 -v $(pwd):/app cff-endpoints:dev
+
+
+# prod -> Use it with WSGI server
+docker run --name flask-endpoints-prod -i -t -p 5000:5000 --restart always cff-endpoints:prod 
+
+```
+
+
+format='%(asctime)s, %(message)s endpoint was reached.', datefmt="%m/%d/%Y %I:%M:%S %p %Z")
--- exercises/python-helloworld/app.py
+++ exercises/python-helloworld/app.py
@@ -1,9 +1,50 @@
-from flask import Flask
+import logging
+from flask import Flask, json
+from flask import jsonify
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
+    app.logger.info("--> default flask logger <-- /")
+    logger.info("/")
     return "Hello World!"
 
+@app.route("/status")
+def status():
+    app.logger.info("--> default flask logger <-- Status")
+    logger.info("Status")
+    return jsonify(result="OK - healthy")
+
+@app.route("/metrics")
+def metrics():
+    r = app.response_class(
+        response = json.dumps({
+            "status":"success",
+            "code":0,
+            "data":{
+                "UserCount":140,
+                "UserCountActive":23
+            }
+            }
+        ),
+        status=200,
+        mimetype="application/json"
+    )
+    app.logger.info("--> default flask logger <-- Metrics")
+    logger.info("Metrics")
+    return r
+
 if __name__ == "__main__":
+    # default log
+    logging.basicConfig(filename='app.log',level=logging.INFO)
+    # custom log  
+    formatter = logging.Formatter('%(asctime)s, %(message)s endpoint was reached.', datefmt="%m/%d/%Y %I:%M:%S %p %Z")
+    handler = logging.FileHandler(filename='app.log')
+    handler.setFormatter(formatter)
+    handler.setLevel(logging.INFO)
+    logger = logging.getLogger('status')    
+    logger.addHandler(handler)
+    logger.propagate = False
+
+                
     app.run(host='0.0.0.0')
